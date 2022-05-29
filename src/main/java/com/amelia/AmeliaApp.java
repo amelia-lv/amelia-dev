cfa5910ef9023886c63de0b47a36dc3e37db0396
Yaroslav Lyskovets <yaroslav.lyskovets@gmail.com>
2022-05-30 00:25:34 +0300

    main class

---
+++ src/main/java/com/amelia/AmeliaApp.java
@@ -0,0 +1,12 @@
+package com.amelia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmeliaApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmeliaApp.class, args);
+    }
+}
